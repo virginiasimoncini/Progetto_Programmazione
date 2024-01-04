df535ad5a6b5886691cfbd1b60e1b17016213b7a
Pablo Tores Rodriguez <66205369+KosmicKatXV@users.noreply.github.com>
2024-01-04 13:42:16 +0100

    Fixed features pipeline

--- fetching.py
+++ fetching.py
@@ -6,11 +6,14 @@ def data_preprocessing():
     breast_cancer_wisconsin_original = fetch_ucirepo(id=15) 
   
     # data (as pandas dataframes) 
-    x = breast_cancer_wisconsin_original.data.features 
-    y = breast_cancer_wisconsin_original.data.targets 
+    x = breast_cancer_wisconsin_original.data.features
+    y = breast_cancer_wisconsin_original.data.targets
+    # normalize features
+    for column in x.columns: 
+        x[column] = (x[column] - x[column].min()) / (x[column].max() - x[column].min())       
 
     # Fixing missing values appropiately (We only analyse the features given that every instance must have a target):
-    x.fillna(method=bfill)
+    x.fillna(method='bfill')
 
     #return the targets and features
     return x,y
