ae3fe8f4a6dabda209e270e9a4e336a42ef38b68
KosmicKatXV <66205369+KosmicKatXV@users.noreply.github.com>
2024-01-15 15:30:15 +0100

    fetching function updated
    It no longer downloads the dataset. Instead it will use a downloaded version

--- fetching.py
+++ fetching.py
@@ -1,19 +1,21 @@
-from ucimlrepo import fetch_ucirepo 
 import pandas as pd
 
 def data_preprocessing():
     # fetch dataset 
-    breast_cancer_wisconsin_original = fetch_ucirepo(id=15) 
+    df = pd.read_csv("breast_cancer.csv") 
   
-    # data (as pandas dataframes) 
-    x = breast_cancer_wisconsin_original.data.features
-    y = breast_cancer_wisconsin_original.data.targets
+    # data (as pandas dataframes)
+    y = df.pop("Class")
+    x = df
     # normalize features
     for column in x.columns: 
         x[column] = (x[column] - x[column].min()) / (x[column].max() - x[column].min())       
 
     # Fixing missing values appropiately (We only analyse the features given that every instance must have a target):
-    x.fillna(method='bfill')
-
+    x.bfill()
+    print(x.head())
+    print(y.head())
     #return the targets and features
     return x,y
+
+data_preprocessing()
