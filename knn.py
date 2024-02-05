e5c37b946afea2932d6983f8cb3e6eeef6f1525a
KosmicKatXV <66205369+KosmicKatXV@users.noreply.github.com>
2024-02-05 12:24:28 +0100

    changed structure of KNN classifier
    It now uses a single function to analyse a single X.

--- knn.py
+++ knn.py
@@ -1,20 +1,17 @@
 import numpy as np
 
 class KNNClassifier:
-    def __init__(self, k):
+    def __init__(self, k, X, y):
         # Inizializza un'istanza del classificatore KNN con il parametro k specificato
         self.k = k
-
-    def fit(self, X, y):
-       # Memorizza i dati di addestramento
         self.X_train = X
-        self.y_train = y
+        self.y_train = y   
 
     def euclidean_distance(self, x1, x2):
         # Calcola la distanza euclidea tra due vettori
         return np.sqrt(np.sum((x1 - x2) ** 2))
 
-    def predict(self, X):
+    def getKClosestLabels(self, X):
         y_pred = []
         for x in X:
             # Calcola le distanze tra il dato di test e tutti i dati di addestramento
@@ -26,6 +23,7 @@ class KNNClassifier:
             # Ottieni le etichette corrispondenti agli indici trovati
             k_labels = [self.y_train[i] for i in k_indices]
 
+        return k_labels
     # Supponiamo che k_labels sia un array contenente le etichette (labels) assegnate a qualche insieme di dati.
 
     # Calcola gli elementi unici e le loro frequenze all'interno dell'array k_labels.
@@ -37,12 +35,11 @@ class KNNClassifier:
     # Crea una lista contenente le etichette più comuni, ovvero quelle con la frequenza massima.
     most_common_labels = [label for label, count in zip(unique_labels, label_counts) if count == max_count]
             
-def predict_most_common_label(k_labels):
-    # Bisogna assicurarsi che k_labels non sia vuoto prima di tentare di trovare l'etichetta più comune
-    if not k_labels:
-        raise ValueError("L'array k_labels è vuoto. Assicurati di fornire dati validi.")
-
-    # Trova l'etichetta più comune tra i k vicini più prossimi
-    most_common_label = max(set(k_labels), key=k_labels.count)
-    
-    return most_common_label
+    def predictX(self, X):
+        k_labels = self.getKClosestLabels(self, X)
+        # Bisogna assicurarsi che k_labels non sia vuoto prima di tentare di trovare l'etichetta più comune
+        if not k_labels:
+            raise ValueError("L'array k_labels è vuoto. Assicurati di fornire dati validi.")
+        # Trova l'etichetta più comune tra i k vicini più prossimi
+        most_common_label = max(set(k_labels), key=k_labels.count)
+        return most_common_label
