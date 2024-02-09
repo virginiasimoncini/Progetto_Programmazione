ab8d7ebe55fd914ec56f69411ee3bea98926684e
GraziaDebora <gd.caldero@alcampus.it>
2024-02-09 12:21:41 +0100

    main

--- main.py
+++ main.py
@@ -6,7 +6,29 @@ from knn import KNNClassifier
 from evaluation import Holdout, XXCrossValidation, ModelEvaluator
 import matplotlib.pyplot as plt
 
-def holdout_validation(X, y, k_neighbors):
+# Carica e pre-processa i dati
+if len(sys.argv) > 1:
+    X, y = data_preprocessing(file=sys.argv[1])
+else:
+    X, y = data_preprocessing()
+
+# Chiedi all'utente di fornire il tipo di validazione
+validation_type = input("Scegli il tipo di validazione (holdout/crossval): ").lower()
+while validation_type not in ['holdout', 'crossval']:
+    print("Scelta non valida. Scegli tra 'holdout' e 'crossval'.")
+    validation_type = input("Scegli il tipo di validazione (holdout/crossval): ")
+
+# Inizializza oggetto di validazione in base alla scelta dell'utente
+if validation_type == 'holdout':
+    # Chiedi all'utente di fornire il numero di vicini solo per Holdout
+    k_neighbors = int(input("Inserisci il numero di vicini (k): "))
+    holdout_validation(X, y, k_neighbors)
+elif validation_type == 'crossval':
+    # Chiedi all'utente di fornire il numero di folds solo per Cross Validation
+    num_folds = int(input("Inserisci il numero di folds per la cross-validation: "))
+    crossval_validation = XXCrossValidation(num_folds=num_folds)
+    evaluator = ModelEvaluator(X, y, validation=crossval_validation, k=None)
+
     # Chiedi all'utente di selezionare le metriche
     print("Scegli le metriche da validare:")
     print("1. Accuracy Rate")
@@ -30,33 +52,7 @@ def holdout_validation(X, y, k_neighbors):
         }
         metrics_to_validate = [metric_mapping.get(metric_choice)]
 
-    holdout_validation = Holdout(test_size=0.2)
-    evaluator = ModelEvaluator(X, y, validation=holdout_validation, k=k_neighbors)
     evaluator.evaluate_validation(metrics=metrics_to_validate)
-
-# Carica e pre-processa i dati
-if len(sys.argv) > 1:
-    X, y = data_preprocessing(file=sys.argv[1])
-else:
-    X, y = data_preprocessing()
-
-# Chiedi all'utente di fornire il tipo di validazione
-validation_type = input("Scegli il tipo di validazione (holdout/crossval): ").lower()
-while validation_type not in ['holdout', 'crossval']:
-    print("Scelta non valida. Scegli tra 'holdout' e 'crossval'.")
-    validation_type = input("Scegli il tipo di validazione (holdout/crossval): ")
-
-# Inizializza oggetto di validazione in base alla scelta dell'utente
-if validation_type == 'holdout':
-    # Chiedi all'utente di fornire il numero di vicini solo per Holdout
-    k_neighbors = int(input("Inserisci il numero di vicini (k): "))
-    holdout_validation(X, y, k_neighbors)
-elif validation_type == 'crossval':
-    # Chiedi all'utente di fornire il numero di folds solo per Cross Validation
-    num_folds = int(input("Inserisci il numero di folds per la cross-validation: "))
-    crossval_validation = XXCrossValidation(num_folds=num_folds)
-    evaluator = ModelEvaluator(X, y, validation=crossval_validation, k=None)
-    evaluator.evaluate_validation()
 else:
     print("Tipo di validazione non riconosciuto. Inserisci 'holdout' o 'crossval'.")
     sys.exit(1)
